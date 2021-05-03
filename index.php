ad30ac438f18eb92de6e1b49cd2223739d427e27
DavidPsencik <davidpsencik459@gmail.com>
2021-05-03 19:10:44 +0200

    first files

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Film storage</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h1>AHOJ</h1>
+
+</body>
+</html>
